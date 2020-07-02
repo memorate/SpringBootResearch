ab5ffe1270eebebe0ef179b7a656e76e1b12ee31
Anchor <sweetsorrow211@gmail.com>
2020-07-02 09:47:48 +0800

    更新.gitignore

--- DailyUse/src/test/java/UnitTest.java
+++ DailyUse/src/test/java/UnitTest.java
@@ -2,13 +2,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.Calendar;
-
 @RunWith(PowerMockRunner.class)
 public class UnitTest {
 
     @Test
     public void test(){
-        Calendar calendar = Calendar.getInstance();
+        int a = 'a';
+        char b = 'b';
+        System.out.println(a);
+        System.out.println(b);
     }
 }
