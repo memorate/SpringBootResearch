1636d97cc20fc6896aaef486e60e70bcf3776b8f
Anchor <sweetsorrow211@gmail.com>
2021-03-11 18:31:25 +0800

    add filter and interceptor

---
+++ SpringBootMybatis/src/main/java/anchor/mybatis/base/filter/AnchorFilter.java
@@ -0,0 +1,25 @@
+package anchor.mybatis.base.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author Anchor
+ */
+public class AnchorFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ SpringBootMybatis/src/main/java/anchor/mybatis/base/interceptor/AnchorInterceptor.java
@@ -0,0 +1,27 @@
+package anchor.mybatis.base.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Anchor
+ */
+public class AnchorInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
